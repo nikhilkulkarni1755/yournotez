33f24f6adc5bed3b570ea820685d925c3149d665
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-11-19 19:44:54 -0800

    redoing it, no need for routing within the app. single page app is enough

--- yournotez/src/app/app.module.ts
+++ yournotez/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
