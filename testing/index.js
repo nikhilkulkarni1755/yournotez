0a134cce05b8ddc1a7a3a0b2273ef62ec61a96d4
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2024-01-09 23:31:06 -0800

    pdf works. need to splot longer notes

--- testing/index.js
+++ testing/index.js
@@ -1,5 +1,7 @@
 // const { default: jsPDF } = require("jspdf")
 
+notes = ['first', 'second', 'third', 'super long note. ling ling practice for 40 hours or twoset will kick your and my ass', 'mozart']
+// https://stackoverflow.com/questions/24272058/word-wrap-in-generated-pdf-using-jspdf
 function generatePDF() {
     console.log('inside fxn')
     pdf = new jsPDF({
@@ -9,8 +11,15 @@ function generatePDF() {
         putOnlyUsedFonts:true
     })
 
-    pdf.text('First pdf created programmatically by nikhil', 20, 20)
-    pdf.text('First pdf created programmatically by nikhil', 20, 30)
+    let counter = 20
+    for(let i = 0; i < notes.length; i++) {
+        // long notes don't work here
+        pdf.text(((i+1) + ") " + notes[i]), 20, counter, {maxWidth: 40})
+        counter += 10
+    }
+
+    // pdf.text('First pdf created programmatically by nikhil', 20, 20)
+    // pdf.text('First pdf created programmatically by nikhil', 20, 30)
     pdf.save('jsPDF.pdf')
 
 
